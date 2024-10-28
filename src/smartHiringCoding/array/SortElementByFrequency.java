f06d727a1a908204f67f6ce67c5c75aee61d9981
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-10-28 12:32:54 +0530

    sort element by frequency is started is going on

--- src/smartHiringCoding/array/SortElementByFrequency.java
+++ src/smartHiringCoding/array/SortElementByFrequency.java
@@ -2,8 +2,9 @@ package smartHiringCoding.array;
 import java.util.*;
 
 public class SortElementByFrequency {
-    static void sortElementByFrequency(int[] arr){
-
+    static int sortElementByFrequency(int[] arr){
+        HashMap<Integer, Integer> list = new HashMap<>();
+        return list.get(1);
     }
     public static void main(String[] args){
         int[] arr = {1,2,3,2,4,3,1,2};
