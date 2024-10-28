4b2d44869a16f9a00cbfc232bf0cc57ec2fb6b2e
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-10-28 14:33:34 +0530

    is being done

--- src/smartHiringCoding/array/SortElementByFrequency.java
+++ src/smartHiringCoding/array/SortElementByFrequency.java
@@ -2,9 +2,27 @@ package smartHiringCoding.array;
 import java.util.*;
 
 public class SortElementByFrequency {
-    static int sortElementByFrequency(int[] arr){
+    static void sortElementByFrequency(int[] arr){
+        Arrays.sort(arr);
         HashMap<Integer, Integer> list = new HashMap<>();
-        return list.get(1);
+        for(int i: arr){
+            if(list.containsKey(i)){
+                list.put(i, list.get(i)+1);
+            }else {
+                list.put(i, 1);
+            }
+        }
+        ArrayList<Integer> temp = new ArrayList<>();
+        for(int i: list.values()){
+            temp.add(i);
+        }
+        Collections.sort(temp, Collections.reverseOrder());
+        for(int i: temp){
+            for(int j=0;j<i;j++){
+                System.out.print(i);
+            }
+            System.out.print(", ");
+        }
     }
     public static void main(String[] args){
         int[] arr = {1,2,3,2,4,3,1,2};
