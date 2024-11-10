0fe80d864a3f35c007d419984f47c4e068a81a67
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-11-10 17:55:07 +0530

    hashset

---
+++ src/collectionFrameworks/setInterfaces/HashSetInJava.java
@@ -0,0 +1,9 @@
+package collectionFrameworks.setInterfaces;
+import java.util.HashSet;
+
+public class HashSetInJava {
+    public static void main(String[] args){
+        HashSet<Integer> set = new HashSet<>();
+
+    }
+}
