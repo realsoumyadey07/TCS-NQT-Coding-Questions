7d548f05b76dbbab0f6e541ada6230a9e764ae15
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-11-19 10:04:02 +0530

    OOPs in java started

---
+++ src/OOPs/OOPs.java
@@ -0,0 +1,47 @@
+package OOPs;
+
+class Pen {
+    String color;
+    String type;
+    public void write(){
+        System.out.println("Writing something");
+    }
+    public void printColor(){
+        System.out.println("The color of the pen is: "+ this.color);
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    public void printName(){
+        System.out.println("Name is: "+ this.name);
+    }
+    public void printAge(){
+        System.out.println("Age is: "+ this.age);
+    }
+    Student(Student s1){
+        this.name = s1.name;
+        this.age = s1.age;
+    }
+}
+
+public class OOPs {
+    public static void main(String[] args){
+        Pen pen1 = new Pen();
+        pen1.color = "Blue";
+        pen1.type = "Gel";
+        pen1.write();
+        pen1.printColor();
+        Pen pen2 = new Pen();
+        pen2.color = "Black";
+        pen2.type = "ballpoint";
+        pen2.printColor();
+        pen2.write();
+        Student student1 = new Student();
+        student1.age = 22;
+        student1.name = "Soumyadip Dey";
+        student1.printAge();
+        student1.printName();
+    }
+}
