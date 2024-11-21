72e3bf39f5b6f77a946eaba91f1d4aa9cc516d7b
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-11-21 12:56:31 +0530

    string questions are being done

---
+++ src/smartHiringCoding/srtings/PrintAscii.java
@@ -0,0 +1,9 @@
+package smartHiringCoding.srtings;
+
+public class PrintAscii {
+    public static void main(String[] args){
+        char a = 'c';
+        int ascaii = a;
+        System.out.println("The ascaii value of a is: "+ ascaii);
+    }
+}
