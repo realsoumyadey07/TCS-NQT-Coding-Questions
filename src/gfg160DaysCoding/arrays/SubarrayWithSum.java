af32a285f3524357da2c10d46e465d306f6c060f
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-12-01 17:16:54 +0530

    SubArray with sum gfg problem solved

--- src/gfg160DaysCoding/arrays/SubarrayWithSum.java
+++ src/gfg160DaysCoding/arrays/SubarrayWithSum.java
@@ -5,21 +5,23 @@ import java.util.Arrays;
 public class SubarrayWithSum {
     static ArrayList<Integer> subarraysum(int[] arr, int target){
         for(int i=0;i<arr.length;i++){
-            int sum = i, j = i;
-            while (sum<target){
-                if(j!=i) {
-                    sum += arr[j];
-                    j++;
-                }
+            int sum = arr[i], j = i+1;
+            while (sum<target && j<arr.length){
+                sum += arr[j];
                 j++;
             }
-            if(sum == target) return new ArrayList<>(Arrays.asList(i, j));
+            if(sum == target) return new ArrayList<>(Arrays.asList(i+1, j));
         }
-        return new ArrayList<>();
+        return new ArrayList<>(Arrays.asList(-1));
     }
     public static void main(String[] args){
-        int[] arr = {1,2,3,7,5};
-        int target = 12;
-        subarraysum(arr, target);
+        int[] arr = {1,2,3,7,5,4,8};
+        int target = 17;
+        ArrayList<Integer> list = subarraysum(arr, target);
+        if(list.getFirst()==-1){
+            System.out.println("No subarray found!");
+        } else {
+            System.out.println("The subarray found between index no. "+ list.getFirst()+ " to "+list.getLast());
+        }
     }
 }
