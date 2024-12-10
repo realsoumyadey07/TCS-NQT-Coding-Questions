d6e1932e8e208076bea273311940f50eb2cd6b21
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-12-10 09:58:41 +0530

    cipher xor program started

---
+++ src/informationSecurityProgramms/CipherXOR.java
@@ -0,0 +1,13 @@
+package informationSecurityProgramms;
+
+public class CipherXOR {
+    static String encrypt(String text, byte key){
+        StringBuilder encrypted = new StringBuilder();
+        return encrypted.toString();
+    }
+    public static void main(String[] args){
+        String text = "Soumya Dey";
+        byte key = 0b00001111;
+        String encrypted = encrypt(text, key);
+    }
+}
